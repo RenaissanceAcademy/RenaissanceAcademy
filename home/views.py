b00bb2228feeff8789b08c258098041a56c4dc31
mousseng <assaeva@gmail.com>
2015-04-07 06:59:38 -0400

    updated views file

--- home/views.py
+++ home/views.py
@@ -8,7 +8,7 @@ def index(request):
     return render(request, 'home/index.html', context)
     
 def about(request):
-    return HttpResponse("Hello world, this is the about page.")
+    return render(request, 'home/about.html', {}) 
 
 def personal(request, name):
-	return HttpResponse("Looking for student id " + name)
+    return render(request, 'home/{}.html'.format(name), {}) 
